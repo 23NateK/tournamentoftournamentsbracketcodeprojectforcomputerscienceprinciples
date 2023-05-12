5e5588ac0ba47e2e7951e2f6d9e9cbdec83ceab9
Ryan Hecht <25ryanhecht@mhs.usd266.com>
2023-05-12 13:59:41 -0500

    adding functionality to the competitor class

--- Competitor.py
+++ Competitor.py
@@ -1,7 +1,24 @@
 class Competitor(object):
     # represents a competitor
     # stores the competitor's:
-        # name
-        # classification (teacher or student)
-        # score
-    def __init__(self, name, classification, score):
+    #   name
+    #   classification (teacher or student)
+    #   score
+    def __init__(self, name, classification, score = 0):
+        # creates a competitor with the given name,
+        # classification, and score
+        self.name = name
+        self.classification = classification
+        self.score = score
+
+    def getName(self):
+        # returns the competitor's name
+        return self.name
+
+    def getScore(self):
+        # returns the competitor's score
+        return self.score
+
+    def addPoints(self, points):
+        # add the inputted number of points to score
+        self.score += points
