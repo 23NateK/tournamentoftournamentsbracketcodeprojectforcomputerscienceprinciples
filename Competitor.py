8545fcfc144dc39de8f3a147c7fd80ee8bbc063b
Ryan Hecht <25ryanhecht@mhs.usd266.com>
2023-05-12 13:50:37 -0500

    starting competitor class

--- Competitor.py
+++ Competitor.py
@@ -1,1 +1,7 @@
-
+class Competitor(object):
+    # represents a competitor
+    # stores the competitor's:
+        # name
+        # classification (teacher or student)
+        # score
+    def __init__(self, name, classification, score):
