3a24dc8fa6c25fcbd8c7269de7834cebdf1f0b49
23NateK <122118163+23NateK@users.noreply.github.com>
2023-05-12 14:04:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,22 @@ from tkinter import *
 
 
 def addFile():
-  print("yes")
+  if pathenterer.contains(".txt"):
+    continue
+  pathsLists.insert(END,'\n'+str(pathenterer))
+  pathsList.add(str(pathenterer))
+  
 def removeFile():
-  print("yes")
+  
 def exportFiles():
   print("yes")
 
 screen = Tk()
+screen.geometry("500x500")
 screen.title("The Tournaments of Tournaments")
 fileadder = Button(screen,text="Add",command=addFile)
 fileremover = Button(screen,text="Remove Last",command=removeFile)
-pathenterer = Entry(screen,text="Enter File Here")
+pathenterer = Entry(screen)
 exportbutton = Button(screen,text="ExportFiles",command=exportFiles)
 exportlabel = Label(screen)
 pathsLists = Label(screen)
@@ -30,6 +35,6 @@ pathenterer.grid(row=1,column=0)
 fileremover.grid(row=0,column=1)
 exportbutton.grid(row=0,column=2)
 exportlabel.grid(row=1,column=1)
-pathsList.grid(row=1,column=2)
-screen.geometry("500x500")
+pathsLists.grid(row=1,column=2)
 screen.mainloop()
+pathsList = []
