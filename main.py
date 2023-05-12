9f6c433341b850dd8544e6e7afffbab03eace184
23NateK <122118163+23NateK@users.noreply.github.com>
2023-05-12 13:32:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 #Main ToDo: GUI
+#add buton
+#revome buton
+#file path inpot
+#export botn
+#export dispblay labl
+
 import tkinter
+screen = Tk()
+screen.title("The Tournaments of Tournaments")
+fileadder = Button(screen,text="Add",command=addFile)
+fileremover = Button(screen,text="Remove Last",command=removeFile)
+pathenterer = Entry(screen,text="Enter File Here")
+exportbutton = Button(screen,text="ExportFiles",command=exportFiles)
+exportlabel = Label(screen)
