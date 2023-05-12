2e1fc6b591cdad91ceb1f59bc335fd5d74bdf650
23NateK <122118163+23NateK@users.noreply.github.com>
2023-05-12 13:44:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 #export dispblay labl
 #
 
-import tkinter
+from tkinter import *
 
 
 
@@ -24,10 +24,12 @@ fileremover = Button(screen,text="Remove Last",command=removeFile)
 pathenterer = Entry(screen,text="Enter File Here")
 exportbutton = Button(screen,text="ExportFiles",command=exportFiles)
 exportlabel = Label(screen)
+pathsLists = Label(screen)
 fileadder.grid(row=0,column=0)
 pathenterer.grid(row=1,column=0)
 fileremover.grid(row=0,column=1)
 exportbutton.grid(row=0,column=2)
 exportlabel.grid(row=1,column=1)
-
+pathsList.grid(row=1,column=2)
+screen.geometry("500x500")
 screen.mainloop()
