cb98be1fcab1ea4a8a7874459eebc779d30e8152
23NateK <122118163+23NateK@users.noreply.github.com>
2023-05-12 13:41:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,19 @@
 #file path inpot
 #export botn
 #export dispblay labl
+#
 
 import tkinter
+
+
+
+def addFile():
+  print("yes")
+def removeFile():
+  print("yes")
+def exportFiles():
+  print("yes")
+
 screen = Tk()
 screen.title("The Tournaments of Tournaments")
 fileadder = Button(screen,text="Add",command=addFile)
@@ -13,3 +24,10 @@ fileremover = Button(screen,text="Remove Last",command=removeFile)
 pathenterer = Entry(screen,text="Enter File Here")
 exportbutton = Button(screen,text="ExportFiles",command=exportFiles)
 exportlabel = Label(screen)
+fileadder.grid(row=0,column=0)
+pathenterer.grid(row=1,column=0)
+fileremover.grid(row=0,column=1)
+exportbutton.grid(row=0,column=2)
+exportlabel.grid(row=1,column=1)
+
+screen.mainloop()
