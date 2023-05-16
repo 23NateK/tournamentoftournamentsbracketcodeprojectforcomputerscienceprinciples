10ab6f3437563bdd317bb114e8a55e77a6e013b1
AidenChar <133048898+AidenChar@users.noreply.github.com>
2023-05-16 13:39:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,23 @@
 
 import Competitor
 from tkinter import *
-pathListLabel = []
+Noahsdownfall = []
 
 
 def addFile():
-  pathListLabel.append(str(pathenterer.get()))
+  Noahsdownfall.append(str(pathenterer.get()))
   pathsLists.config(text="")
   for i in range(0,len(pathsList)-1):
     pathsLists.insert(END,'\n'+str(pathsList[i]))
   
 def removeFile():
-  pathListLabel.pop(len(pathsList)-1)
+  Noahsdownfall.pop(len(pathsList)-1)
   pathsLists.clear()
   for i in range(0,len(pathsList)-1):
     pathsLists.insert(END,'\n'+str(pathsList[i]))
 def exportFiles():
   competitorList = []
-  for i in pathListLabel:
+  for i in Noahsdownfall:
     text = open(i,'r')
     #make the code here to create an object named "person" that interprets the text file's name and subclass
     readables = text.readlines() #this returns a list with each row in the file being an object in that list
