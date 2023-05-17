4fe7c9d72ec1e8f96207dd4d38a0c89f945c0fcb
Ryan Hecht <25ryanhecht@mhs.usd266.com>
2023-05-17 13:22:58 -0500

    working on importResponse.py

--- importResponse.py
+++ importResponse.py
@@ -1,2 +1,14 @@
 def importResponse(responseFile):
     # import a competitor's response from a file
+    response = []
+
+    # open the file & store lines in a list
+    with open(responseFile) as file:
+        lines = [line.rstrip() for line in file]
+
+    response.append(lines[0]) # append the name to the response
+
+    for i in range(2, 13, 2): # loop through the lines with answers
+        response.append(lines[i].split(', '))
+
+    return response
